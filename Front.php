73aa3c0697e73e1cd6cc9ae8902960245df756e3
Mohammad Fathi <mimfapost@gmail.com>
2025-03-01 20:50:32 +0330

    Fixed some bugs

--- Front.php
+++ Front.php
@@ -1,5 +1,5 @@
 <?php
-class Template extends FrontBase{
+class Front extends FrontBase{
 	public $ForeColorPalette = array("#151515","#202020","#101010");
 	public $BackColorPalette = array("#fdfeff","#fafbfc","#fdfeff");
 	public $FontPalette = array("'dubai light', sans-serif","'dubai', sans-serif","'dubai', sans-serif");
