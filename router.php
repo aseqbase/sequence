f35639b52fbe9ea0ac37bc51bcde7b66cd80de42
Mohammad Fathi <mimfapost@gmail.com>
2025-07-23 21:31:17 +0330

    v3.3.0

--- router.php
+++ router.php
@@ -1,12 +1,12 @@
 <?php
 // To unset the default router sat at the bottom layers
-\_::$Back->Router->Route->Reset();
+\_::$Back->Router->On()->Reset();
 
 /**
  * Use your routers by below formats
- * \_::$Back->Router->Route("A Part Of Path?")->Default("Route Name");
+ * \_::$Back->Router->On("A Part Of Path?")->Default("Route Name");
  */
 
 // To route other requests to the DefaultRouteName
-\_::$Back->Router->Route->Default(\_::$Config->DefaultRouteName);
+\_::$Back->Router->On()->Default(\_::$Config->DefaultRouteName);
 ?>
